98baf870d542743103eed39443546188e2412ec1
akihiro17 <coolwizard11@gmail.com>
2016-11-02 16:11:38 +0900

    add files

--- .emacs.d/init/init-js2-mode.el
+++ .emacs.d/init/init-js2-mode.el
@@ -1,3 +1,4 @@
 (add-hook 'js2-mode-hook (lambda () (setq js2-basic-offset 2)))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-jsx-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-jsx-mode))
 (setq-default indent-tabs-mode nil)
