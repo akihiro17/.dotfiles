98fa92dea79548412f1356544f8cd1765fc9bd83
akihiro17 <coolwizard11@gmail.com>
2017-08-07 02:02:58 +0900

    go mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,10 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (when load-file-name
   (setq user-emacs-directory (file-name-directory load-file-name)))
 
@@ -41,7 +48,8 @@
 (el-get-bundle ruby-mode)
 (el-get-bundle sass-mode)
 (el-get-bundle slim-mode)
-(el-get-bundle elixir-mode)
+(el-get-bundle elixir)
+(el-get-bundle go-mode)
 (el-get-bundle tuareg) ;; ocaml
 
 ;; ruby specific settings
